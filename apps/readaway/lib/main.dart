b91d182b33e2424e4a9f1bc68ce7989319d508c4
DrkXo <chayantaposh@gmail.com>
2026-09-07 18:17:29 +0530

    chore(App): added runZonedGuarded

--- apps/readaway/lib/main.dart
+++ apps/readaway/lib/main.dart
@@ -1,40 +1,49 @@
+import 'dart:async';
+
 import 'package:flutter/foundation.dart'
     show LicenseEntryWithLineBreaks, LicenseRegistry;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
-import 'package:timezone/data/latest_all.dart' as tz;
-
 import 'package:get_it/get_it.dart';
+import 'package:timezone/data/latest_all.dart' as tz;
 
 import 'src/app.dart';
 import 'src/core/config/injection.dart';
 import 'src/core/services/file_open_service.dart';
 
 Future<void> main([List<String> args = const []]) async {
-  WidgetsFlutterBinding.ensureInitialized();
+  runZonedGuarded(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
 
-  LicenseRegistry.addLicense(() async* {
-    for (final family in [
-      'NotoSerif',
-      'NotoSans',
-      'FiraCode',
-      'JetBrainsMono',
-    ]) {
-      yield LicenseEntryWithLineBreaks(
-        ['google_fonts'],
-        await rootBundle.loadString('assets/google_fonts/OFL-$family.txt'),
-      );
-    }
-  });
+      LicenseRegistry.addLicense(() async* {
+        for (final family in [
+          'NotoSerif',
+          'NotoSans',
+          'FiraCode',
+          'JetBrainsMono',
+        ]) {
+          yield LicenseEntryWithLineBreaks(
+            ['google_fonts'],
+            await rootBundle.loadString('assets/google_fonts/OFL-$family.txt'),
+          );
+        }
+      });
 
-  // Initialise the timezone database used for scheduling notifications.
-  tz.initializeTimeZones();
+      // Initialise the timezone database used for scheduling notifications.
+      tz.initializeTimeZones();
 
-  await configureDependencies();
+      await configureDependencies();
 
-  if (args.isNotEmpty) {
-    GetIt.I<FileOpenService>().initializeWithArgs(args);
-  }
+      if (args.isNotEmpty) {
+        GetIt.I<FileOpenService>().initializeWithArgs(args);
+      }
 
-  runApp(const ReadAway());
+      runApp(const ReadAway());
+    },
+    (error, stackTrace) {
+      // crashAnalytics.onUncaughtError(error, stackTrace);
+      debugPrint('UNCAUGHT ERROR: $error\n$stackTrace');
+    },
+  );
 }
